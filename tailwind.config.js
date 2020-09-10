58090d5095f8b0a08b0fe8f60654476e3a6c477b
Vihan Chaudhry <vihanchaudhry@gmail.com>
2020-09-11 01:51:16 +0530

    Add purge template paths to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx,css}',
+    './components/**/*.{js,ts,jsx,tsx,css}',
+    './styles/**/*.{js,ts,jsx,tsx,css}',
+  ],
   theme: {
     extend: {},
   },
